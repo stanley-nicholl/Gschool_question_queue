e9fdf68366e5b358dcfa8566a0897c1b011b1d57
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-03 12:32:47 -0700

    Testing pre-commit hook

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,9 @@ const config = require('./config')
 const firebase = require('firebase')
 
 firebase.initializeApp(config)
-let user = firebase.auth().currentUser;
+let user = firebase.auth().currentUser
 
-if(user) {
+if (user) {
   console.log('Someone is signed in!')
 } else {
   console.log('No one is currently signed in.')
