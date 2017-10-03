2363d6a263372edf275fb8735ed5e5af7a05683e
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-03 12:11:46 -0700

    Start hooking in Firebase

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
 const css = require('../styles/app.scss') // eslint-disable-line
+const config = require('./config')
+const firebase = require('firebase')
+
+firebase.initializeApp(config)
+let user = firebase.auth().currentUser;
+
+if(user) {
+  console.log('Someone is signed in.')
+} else {
+  console.log('No one is currently signed in.')
+}
