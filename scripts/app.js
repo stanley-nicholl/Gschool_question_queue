a428d69c9080e89e6c6136f1cdf4285a1393fa7e
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-04 16:40:10 -0700

    Pull all items from server and populate list; updated list into ul/li instead of div

--- scripts/app.js
+++ scripts/app.js
@@ -12,10 +12,10 @@ let name, request
 function getRequests (requestID) {
   database.ref('requests/' + requestID).once('value').then(snapshot => {
     let result = snapshot.val()
-    console.log(snapshot.val())
+    // console.log(snapshot.val())
     name = result.name
     request = result.question
-    console.log('name', name, 'request', request)
+    // console.log('name', name, 'request', request)
 
     const element = document.getElementById('queue1')
     element.innerHTML = `
@@ -36,4 +36,33 @@ function getRequests (requestID) {
   // console.log(requests)
 }
 
-getRequests(2)
+function getAllRequests() {
+  database.ref('requests/').once('value').then(snapshot => {
+    let result = snapshot.val()
+    result.forEach(function(item) {
+      // console.log(item)
+      const newListItem = document.createElement("LI")
+      newListItem.className = "row entry py-2"
+      newListItem.innerHTML = `
+      <div class="col-md-1 d-flex justify-content-center align-items-center">
+        <p class="element queueNum">${item._id}</p>
+      </div>
+      <div class="col-md-3 d-flex align-items-center">
+        <p class="element name">${item.name}</p>
+      </div>
+      <div class="col-md-6 d-flex align-items-center">
+        <p class="element topic">${item.question}</p>
+      </div>
+      <div class="col-md-2 d-flex align-items-center justify-content-center">
+        <p class="element resolve"><i class="fa fa-check-circle" aria-hidden="true"></i></p>
+      </div>
+      `
+      document.getElementById('queue').appendChild(newListItem)
+    })
+  })
+}
+
+getAllRequests()
+
+
+// getRequests(3)
