9185e5c25de306c54a9e4a983e71b07d696109a0
Wes Reid <bwreid@gmail.com>
2017-10-06 13:29:41 -0700

    Restructure files and requires for build with browserify
    - Build project for use with browserify
    - Copy over pages/ folder to dist/ folder in build script
    - Update paths for links and assets
    - Bundle all files and .scss into bundle.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,26 @@
-let userInfo = {}
-let submit = document.getElementById('submit')
-let repeatUser = window.localStorage.getItem('repeatUser')
-
 function detect () {
+  let repeatUser = window.localStorage.getItem('repeatUser')
   if (repeatUser) {
-    window.location.href = 'askify.html'
+    window.location.pathname = '/pages/askify.html'
   }
 }
 
-detect()
+if (window.location.pathname === '/') {
+  let userInfo = {}
+  let submit = document.getElementById('submit')
 
-submit.addEventListener('click', function (e) {
-  e.preventDefault()
-  userInfo['fname'] = document.getElementById('fname').value
-  userInfo['lname'] = document.getElementById('lname').value
-  userInfo['email'] = document.getElementById('email').value
-  window.localStorage.setItem('repeatUser', 'yes')
-  window.localStorage.setItem('userFName', document.getElementById('fname').value)
-  window.location.href = 'askify.html'
-  window.localStorage.setItem('repeatUser', 'yes')
-  window.localStorage.setItem('user', JSON.stringify(userInfo))
-  window.location.href = 'askify.html'
-})
+  detect()
+
+  submit.addEventListener('click', function (e) {
+    e.preventDefault()
+    userInfo['fname'] = document.getElementById('fname').value
+    userInfo['lname'] = document.getElementById('lname').value
+    userInfo['email'] = document.getElementById('email').value
+    window.localStorage.setItem('repeatUser', 'yes')
+    window.localStorage.setItem('userFName', document.getElementById('fname').value)
+    window.location.pathname = '/pages/askify.html'
+    window.localStorage.setItem('repeatUser', 'yes')
+    window.localStorage.setItem('user', JSON.stringify(userInfo))
+    window.location.pathname = '/pages/askify.html'
+  })
+}
