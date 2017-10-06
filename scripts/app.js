b666fe21825b47f6448cc20c90d5e70fbd534b63
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-06 11:33:07 -0700

    Refactor index.js into app.js

--- scripts/app.js
+++ scripts/app.js
@@ -244,11 +244,32 @@ function displayArchivedQuestions () {
   })
 }
 
-// writeUserData(2, "Stan N.", 'stan@example.com')
+function detect () {
+  let repeatUser = window.localStorage.getItem('repeatUser')
+  if (repeatUser) {
+    window.location.href = 'askify.html'
+  }
+}
 
 if (window.route === 'index') {
   // do login page stuff
+  let userInfo = {}
+  let submit = document.getElementById('submit')
 
+  detect()
+
+  submit.addEventListener('click', function (e) {
+    e.preventDefault()
+    userInfo['fname'] = document.getElementById('fname').value
+    userInfo['lname'] = document.getElementById('lname').value
+    userInfo['email'] = document.getElementById('email').value
+    window.localStorage.setItem('repeatUser', 'yes')
+    window.localStorage.setItem('userFName', document.getElementById('fname').value)
+    window.location.href = 'askify.html'
+    window.localStorage.setItem('repeatUser', 'yes')
+    window.localStorage.setItem('user', JSON.stringify(userInfo))
+    window.location.href = 'askify.html'
+  })
 } else if (window.route === 'askify') {
   let userInfo = JSON.parse(window.localStorage.getItem('user'))
   username = userInfo.fname
